06ff01e057805bc263978b51d17de9ad532d0cb7
Masatoshi <msts.oo0131@gmail.com>
2021-10-01 15:38:44 +0900

    Limit_A

--- Q.24.sql
+++ Q.24.sql
@@ -1,6 +1,4 @@
-select name,height,weight from players order By height desc limit 5;
-
-→Limitを利用することで上位分の数字を出力することが可能になる。
+select name,height,weight from players order By height desc limit 6,15 ;
 
 尚、betweenに関してはwhereで条件分岐していないとできない。
 パフォーマンス等に関しては、whereを使うbetweenを利用する方が早いと言われている。
